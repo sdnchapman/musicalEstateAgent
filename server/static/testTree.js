17d0a49d2fb314cbdf8a1a64c99505f2a363c930
Joshua Allwood <jjallwood@live.co.uk>
2019-01-25 22:22:46 +0000

    Added socket logic to sample page on server

--- server/static/testTree.js
+++ server/static/testTree.js
@@ -2,3 +2,9 @@
  * Created by clark on 25/01/2019.
  */
 console.log('Test file loaded');
+var socket = io('http://localhost');
+
+socket.on('hello', function (length) {
+    console.log(length);
+    socket.emit('my other event', { my: 'data' });
+});
