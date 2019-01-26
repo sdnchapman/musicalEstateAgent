03a35cdcc3616849fa1c8a7953ad625ba9379a99
Joshua Allwood <jjallwood@live.co.uk>
2019-01-26 00:17:42 +0000

    Added react router to client

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Login from './Login';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import "./styles.scss";
 
 const App = () => {
-  return (
-    <div className="app-container">
-      <Login/>
-    </div>
-  );
+    return (
+        <div className="app-container">
+            <Router>
+                <Route path="/" exact component={Login}/>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
