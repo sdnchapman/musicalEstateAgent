b8832885e5d10c430c34c6b1f407750758fed6c9
Joshua Allwood <jjallwood@live.co.uk>
2019-01-26 01:24:57 +0000

    Removed static folder from server folder. Now needs to be generated from client. * Updated webpack to build project into server folder.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   devtool: 'inline-source-map',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../server/static/'),
     filename: 'bundle.js'
   },
   plugins: [htmlWebpackPlugin],
