844e8713f82bc171a21db9ffc82b330480722953
Joshua Allwood <jjallwood@live.co.uk>
2019-01-27 13:28:33 +0000

    ConductorSetup instructions have been streamlined.

--- client/src/components/ConductorSetup.jsx
+++ client/src/components/ConductorSetup.jsx
@@ -56,45 +56,38 @@ export default class ConductorSetup extends Component {
           {
             (screen === 1) && (
               <React.Fragment>
-                <p>You are the conductor. You will lead the orchestra is today's house valuation.</p>
-                <h3>Stand Up and take a bow!</h3>
-                <p>Ready to get to business? Click the next arrow!</p>
+                <p>You are the conductor.</p>
+                <h3>Stand up and take a bow!</h3>
+                <p>Ready to get to business? Click the next button!</p>
               </React.Fragment>
             )
           }
           {
             (screen === 2) && (
               <React.Fragment>
-                <p>In your head split your audience into 3 groups.</p>
-                <p>In the next stage you will assign each group a colour. Red, Green and Blue.</p>
+                <p>Split the rest of the players into 3 groups.</p>
+                <p>If this is your first game, we recommend splitting them into left, centre and right.</p>
               </React.Fragment>
             )
           }
           {
             (screen === 3) && (
               <React.Fragment>
-                <p>Lets begin with the Red Team.</p>
-                <p>Point in the direction of your first group and announce that they are the 'Red Team'.</p>
-                <p>They will then register this into their App.</p>
+                <p>Point in the direction of your first group (left) and announce that they are the 'Red Team'.</p>
               </React.Fragment>
             )
           }
           {
             (screen === 4) && (
               <React.Fragment>
-                <p>Now onto the Green team.</p>
-                <p>Point in the direction of your second group and announce that they are in the 'Green Team'.</p>
-                <p>They will then register this into their App.</p>
+                <p>Point in the direction of your second group (center) and announce that they are in the 'Green Team'.</p>
               </React.Fragment>
             )
           }
           {
             (screen === 5) && (
               <React.Fragment>
-                <p>Now onto the Blue team.</p>
-                <p>Point in the direction of your third and final group and announce that they are in the 'Blue
-                  Team'.</p>
-                <p>They will then register this into their App.</p>
+                <p>Point in the direction of your third group (right) and announce that they are in the 'Blue Team'.</p>
               </React.Fragment>
             )
           }
@@ -109,14 +102,24 @@ export default class ConductorSetup extends Component {
           {
             (screen === 7) && (
               <React.Fragment>
-                <p>Today is a big day!</p>
+                <p>During the performance you will see 3 coloured tracks.</p>
+                <p>Each track represents one of your groups (Red - Left, Green - Center, Blue - Right).</p>
+                <p>When a note hits the bottom bar on the screen point / gesticulate to that group and they will play their instrument.</p>
               </React.Fragment>
             )
           }
           {
             (screen === 8) && (
               <React.Fragment>
-                <p>When you are ready to start playing, click the button below.</p>
+                <p>Today is a big day for you!</p>
+                <p>Good luck!</p>
+              </React.Fragment>
+            )
+          }
+          {
+            (screen === 9) && (
+              <React.Fragment>
+                <p>When you are ready to start playing, select a song from below.</p>
                 <button className="btn btn-warning" onClick={() => this.onStartGame(0)}>Begin the easy song!</button>
                 <button className="btn btn-danger" onClick={() => this.onStartGame(1)}>Begin the hard song!</button>
               </React.Fragment>
@@ -124,7 +127,7 @@ export default class ConductorSetup extends Component {
           }
         </div>
         {
-          screen < 8 && <button className="btn btn-primary" onClick={this.onNextScreen}>Next</button>
+          screen < 9 && <button className="btn btn-primary" onClick={this.onNextScreen}>Next</button>
         }
       </div>
     );
